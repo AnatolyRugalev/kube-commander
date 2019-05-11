0e6aac752443067b18933cb7a7e3caf93fe1afe0
Anatoly Rugalev <anatoliy.rugalev@gs-labs.ru>
2019-05-11 18:13:07 +0300

    POC pods list and menu selector

--- internal/tui/tui.go
+++ internal/tui/tui.go
@@ -2,6 +2,7 @@ package tui
 
 import (
 	"github.com/AnatolyRugalev/kube-commander/internal/cfg"
+	"github.com/AnatolyRugalev/kube-commander/internal/kube"
 	"github.com/spf13/cobra"
 	"log"
 
@@ -24,15 +25,76 @@ func Start() {
 	}
 	defer ui.Close()
 
-	p := widgets.NewParagraph()
-	p.Text = "Hello World!"
-	p.SetRect(0, 0, 25, 5)
+	client, err := kube.GetClient()
+	if err != nil {
+		log.Fatalf("Kube error")
+	}
 
-	ui.Render(p)
+	grid := ui.NewGrid()
+	termWidth, termHeight := ui.TerminalDimensions()
+	grid.SetRect(0, 0, termWidth, termHeight)
 
-	for e := range ui.PollEvents() {
-		if e.Type == ui.KeyboardEvent {
-			break
+	menuList := widgets.NewList()
+	menuList.Title = "Cluster"
+	menuList.Rows = []string{
+		"Overview",
+		"Namespaces",
+		"Nodes",
+		"Storage Classes",
+		"Persistent Volumes",
+	}
+	menuList.SelectedRow = 0
+	menuList.TextStyle = ui.NewStyle(ui.ColorYellow)
+	menuList.WrapText = false
+
+	pods, err := client.GetPods("")
+
+	if err != nil {
+		log.Fatalf("Pods error")
+	}
+
+	podTable := widgets.NewTable()
+	podTable.Title = "Pods"
+	podTable.RowSeparator = false
+
+	for _, pod := range pods.Items {
+		podTable.Rows = append(podTable.Rows, []string{
+			pod.Name,
+			pod.Namespace,
+			string(pod.Status.Phase),
+		})
+	}
+	menuList.SelectedRow = 0
+	menuList.TextStyle = ui.NewStyle(ui.ColorYellow)
+	menuList.WrapText = false
+
+	grid.Set(
+		ui.NewRow(1.0,
+			ui.NewCol(0.1, menuList),
+			ui.NewCol(0.9, podTable),
+		),
+	)
+
+	ui.Render(grid)
+	uiEvents := ui.PollEvents()
+	for {
+		select {
+		case e := <-uiEvents:
+			switch e.ID {
+			case "q", "<C-c>":
+				return
+			case "<Down>":
+				menuList.SelectedRow += 1
+				ui.Render(grid)
+			case "<Up>":
+				menuList.SelectedRow -= 1
+				ui.Render(grid)
+			case "<Resize>":
+				payload := e.Payload.(ui.Resize)
+				grid.SetRect(0, 0, payload.Width, payload.Height)
+				ui.Clear()
+				ui.Render(grid)
+			}
 		}
 	}
 }
