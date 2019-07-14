236ea088b9c8c1eaeb94a6161ac90074f0d864d3
Anatoly Rugalev <anatoliy.rugalev@gs-labs.ru>
2019-07-14 22:41:38 +0300

    fix(travis): remove -noci flag from semantic-release

--- release.sh
+++ release.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-~/bin/semantic-release -ghr -vf -noci
+~/bin/semantic-release -ghr -vf
 export VERSION=$(cat .version)
 cd cmd/kube-commander
 CGO_ENABLED=0 gox -os '!freebsd !netbsd' -arch '!arm' -ldflags="-s -w -X main.version=${VERSION}" -output="../../bin/{{.Dir}}_v"$VERSION"_{{.OS}}_{{.Arch}}"
