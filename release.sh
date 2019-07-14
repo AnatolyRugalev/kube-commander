241166252b4b5c40ef335786d273bde39ee67170
Anatoly Rugalev <anatoliy.rugalev@gs-labs.ru>
2019-07-14 21:42:08 +0300

    semantic release configuration

--- release.sh
+++ release.sh
@@ -4,6 +4,6 @@ set -e
 
 ~/bin/semantic-release -ghr -vf -noci
 export VERSION=$(cat .version)
-CGO_ENABLED=0 gox -os '!freebsd !netbsd !windows' -arch '!arm' -ldflags="-s -w -X main.version=${VERSION}" -output="bin/{{.Dir}}_v"$VERSION"_{{.OS}}_{{.Arch}}"
-#ghr $(cat .ghr) bin/
-#24d3d2fc8445a67c17f8b7f09846c3a32518e366
+cd cmd/kube-commander
+CGO_ENABLED=0 gox -os '!freebsd !netbsd !windows' -arch '!arm' -ldflags="-s -w -X main.version=${VERSION}" -output="../../bin/{{.Dir}}_v"$VERSION"_{{.OS}}_{{.Arch}}"
+ghr $(cat .ghr) bin/
