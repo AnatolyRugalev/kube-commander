e7951cad37a3ed6bae9f8d568688a4bbb89835f6
Anatoly Rugalev <anatoly.rugalev@gmail.com>
2020-08-26 01:04:30 +0300

    fix(ui): add more core resources on kube-commander init stage

--- app/client/resource_provider.go
+++ app/client/resource_provider.go
@@ -2,33 +2,63 @@ package client
 
 import (
 	"github.com/AnatolyRugalev/kube-commander/commander"
-	v1 "k8s.io/api/core/v1"
+	apps "k8s.io/api/apps/v1"
+	batch "k8s.io/api/batch/v1"
+	batchBeta "k8s.io/api/batch/v1beta1"
+	core "k8s.io/api/core/v1"
+	storageBeta "k8s.io/api/storage/v1beta1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 	"strings"
 )
 
-var coreResources = commander.ResourceMap{
-	"Node":                  {Namespaced: false},
-	"Namespace":             {Namespaced: false},
-	"PersistentVolume":      {Namespaced: false},
-	"Pod":                   {Namespaced: true},
-	"ConfigMap":             {Namespaced: true},
-	"Secret":                {Namespaced: true},
-	"Service":               {Namespaced: true},
-	"ServiceAccount":        {Namespaced: true},
-	"PersistentVolumeClaim": {Namespaced: true},
+var inititalResources = map[schema.GroupVersion]map[string]bool{
+	core.SchemeGroupVersion: {
+		"Node":                  false,
+		"Namespace":             false,
+		"PersistentVolume":      false,
+		"Pod":                   true,
+		"ConfigMap":             true,
+		"Secret":                true,
+		"Service":               true,
+		"ServiceAccount":        true,
+		"PersistentVolumeClaim": true,
+	},
+	apps.SchemeGroupVersion: {
+		"Deployment":  true,
+		"StatefulSet": true,
+		"DaemonSet":   true,
+		"ReplicaSet":  true,
+	},
+	batch.SchemeGroupVersion: {
+		"Job": true,
+	},
+	batchBeta.SchemeGroupVersion: {
+		"CronJob": true,
+	},
+	storageBeta.SchemeGroupVersion: {
+		"StorageClass": false,
+	},
 }
+
+var coreResources = commander.ResourceMap{}
 
 func init() {
 	// Prepare resource names
-	for kind, res := range coreResources {
-		resName := strings.ToLower(kind)
-		if strings.HasSuffix(resName, "s") {
-			resName += "es"
-		} else {
-			resName += "s"
+	for gv, resMap := range inititalResources {
+		for kind, namespaced := range resMap {
+			resName := strings.ToLower(kind)
+			if strings.HasSuffix(resName, "s") {
+				resName += "es"
+			} else {
+				resName += "s"
+			}
+			res := commander.Resource{
+				Namespaced: namespaced,
+				Resource:   resName,
+				Gvk:        gv.WithKind(kind),
+			}
+			coreResources[kind] = &res
 		}
-		res.Resource = resName
-		res.Gvk = v1.SchemeGroupVersion.WithKind(kind)
 	}
 }
 
